e659e53d3b3b0a2c018ad19639d92b204cb2dcbd
StarsMr <ChenikQing@outlook.com>
2021-09-13 15:18:21 +0800

    添加redis数据库

--- StarsBlog/StarsBlog/settings.py
+++ StarsBlog/StarsBlog/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-sgskt)36#8&&%(@e6o1v56y0g7ney@ccmz7+dz*6iqcvi2a*9b
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'StarsBlog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -82,12 +79,12 @@ WSGI_APPLICATION = 'StarsBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
-        'NAME': 'DB_Blog',                      # 数据库名
-        'USER': 'root',                         # 账号
-        'PASSWORD': '12345',                    # 密码
-        'HOST': '127.0.0.1',                    # HOST
-        'PORT': '3306'                          # 端口
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'NAME': 'DB_Blog',  # 数据库名
+        'USER': 'root',  # 账号
+        'PASSWORD': '12345',  # 密码
+        'HOST': '127.0.0.1',  # HOST
+        'PORT': '3306'  # 端口
     }
 }
 
@@ -109,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -123,7 +119,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -133,3 +128,23 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": 'django_redis.client.DefaultClient',
+        }
+    },
+    "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": 'django_redis.client.DefaultClient',
+        }
+    },
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
